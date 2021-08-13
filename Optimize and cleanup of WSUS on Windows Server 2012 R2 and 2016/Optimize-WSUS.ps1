1ebbad3863a7bbd73b29584ab17866f1da4facca
juangranados <juangranados@gmail.com>
2021-08-13 18:01:53 +0200

    Add files via upload

---
+++ Optimize and cleanup of WSUS on Windows Server 2012 R2 and 2016/Optimize-WSUS.ps1
@@ -0,0 +1,31 @@
+﻿<#
+.SYNOPSIS
+	Optimize WSUS DB and performs maintenance.
+
+.DESCRIPTION
+	Optimize WSUS DB using official Microsoft SQL script and performs server maintenance.
+    
+    WsusDBMaintenance.sql and script must be on the same path.
+    
+    Works in Server 2019, 2016 and 2012 R2.
+
+    Saves log in script path: yyyyMMdd__Optimize-WSUS.log
+      
+    Prerequisites for running Invoke-Sqlcmd
+
+    https://www.microsoft.com/en-US/download/details.aspx?id=55992
+    
+    Install packages in order:
+        1. SQLSysClrTypes.msi
+        2. SharedManagementObjects.msi  
+        3. Run from PowerShell: Install-Module -Name SqlServer
+    
+    .NOTES 
+	Author: Juan Granados 
+#>
+#Requires -RunAsAdministrator
+$scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
+Start-Transcript "$scriptPath\$(Get-Date -format "yyyyMMdd")_Optimize-WSUS.log"
+Get-WsusServer | Invoke-WsusServerCleanup -CleanupObsoleteComputers -CleanupObsoleteUpdates -CleanupUnneededContentFiles -CompressUpdates -DeclineExpiredUpdates -DeclineSupersededUpdates
+Invoke-Sqlcmd -ServerInstance "\\.\pipe\microsoft##WID\tsql\query" -InputFile "$scriptPath\WsusDBMaintenance.sql" -Verbose
+Stop-Transcript
---
+++ Optimize and cleanup of WSUS on Windows Server 2012 R2 and 2016/readme.md
@@ -0,0 +1,6 @@
+# Optimize and cleanup of WSUS on Windows Server
+
+Optimize WSUS DB using [ official Microsoft SQL script](https://web.archive.org/web/20200318183528/https://gallery.technet.microsoft.com/scriptcenter/6f8cde49-5c52-4abd-9820-f1d270ddea61) and performs server maintenance.
+
+WsusDBMaintenance.sql included in zip and script must be on the same path.
+
