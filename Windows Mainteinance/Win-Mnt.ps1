a37a9580cc3133729ead7ad7d1dd1cdcdfca7f24
Juan Granados <juangranados@gmail.com>
2021-10-05 08:41:08 +0200

    Add files via upload

--- Windows Mainteinance/Win-Mnt.ps1
+++ Windows Mainteinance/Win-Mnt.ps1
@@ -7,6 +7,9 @@
 .PARAMETER all
     Runs all.
     Default: false
+.PARAMETER antivirus
+    Runs all antivirus: defender, Karspersky, McAfee, ClamAV and Adaware.
+    Default: false
 .PARAMETER sfc
     Runs SFC /scannow.
     Default: false
@@ -31,12 +34,21 @@
 .PARAMETER kas
     Runs Kaspersky Virus Removal Tool
     Default: false
+.PARAMETER clamav
+    Runs ClamAV full scan of C:\
+    Default: false
+.PARAMETER mcafee
+    Runs McAfee Stinger
+    Default: false
 .PARAMETER LogPath
     Path where save log file.
     Default: My Documents
 .EXAMPLE
     Runs all commands.
     Win-Mnt.ps1 -all
+.EXAMPLE
+    Runs all antivirus.
+    Win-Mnt.ps1 -antivirus
 .EXAMPLE
     Runs sfc and defrag with custom log.
     Win-Mnt.ps1 -sfc -defrag -logPath "\\INFSRV001\Scripts$\Mainteinance\Logs"
@@ -62,6 +74,12 @@ Param(
     [switch]$adaware,
     [Parameter()] 
     [switch]$kas,
+    [Parameter()] 
+    [switch]$mcafee,
+    [Parameter()] 
+    [switch]$antivirus,
+    [Parameter()]
+    [switch]$clamav,
     [Parameter()] 
     [switch]$defrag,
     [Parameter()] 
@@ -99,7 +117,7 @@ if ($wmi -or $all) {
     Winmgmt /salvagerepository
 
 }
-if ($defender -or $all) {
+if ($defender -or $all -or $antivirus) {
     Write-Host "------------------------------------------------"
     Write-Host "Updating and running Windows Defender Quick Scan"
     Write-Host "------------------------------------------------"
@@ -114,7 +132,7 @@ if ($defender -or $all) {
         Write-Output "An error has ocurred: $($_.Exception.Message)"
     } 
 }
-if ($adaware -or $all) {
+if ($adaware -or $all -or $antivirus) {
     Write-Host "-----------------------------------------"
     Write-Host "Downloading, updating and running adaware"
     Write-Host "-----------------------------------------"
@@ -152,7 +170,7 @@ if ($adaware -or $all) {
         Write-Host "Error downloading adawareCommandLineScanner"
     }
 }
-if ($kas -or $all) {
+if ($kas -or $all -or $antivirus) {
     Write-Host "----------------------------"
     Write-Host "Downloading and running KVRT"
     Write-Host "----------------------------"
@@ -160,10 +178,58 @@ if ($kas -or $all) {
     $(New-Object System.Net.WebClient).DownloadFile("https://devbuilds.s.kaspersky-labs.com/devbuilds/KVRT/latest/full/KVRT.exe","$env:LOCALAPPDATA\Temp\KVRT.exe")
     if (Test-Path "$env:LOCALAPPDATA\Temp\KVRT.exe") {
         Write-Host "Running KVRT"
-        $kvrtProcess = Start-Process "$env:LOCALAPPDATA\Temp\KVRT.exe" "-accepteula -adinsilent -silent -processlevel 2 -fupdate" -Wait -NoNewWindow -RedirectStandardOutput "$($logPath)\$($timestamp)_$($env:COMPUTERNAME)_kvrt_scan.txt"
+        Start-Process "$env:LOCALAPPDATA\Temp\KVRT.exe" "-accepteula -adinsilent -silent -processlevel 2 -fupdate" -Wait -NoNewWindow -RedirectStandardOutput "$($logPath)\$($timestamp)_$($env:COMPUTERNAME)_kvrt_scan.txt"
         Get-Content -Path "$($logPath)\$($timestamp)_$($env:COMPUTERNAME)_kvrt_scan.txt"
     } else {
         Write-Host "Error downloading KVRT"
+    }
+}
+if ($mcafee -or $all -or $antivirus) {
+    Write-Host "--------------------------------------"
+    Write-Host "Downloading and running McAfee Stinger"
+    Write-Host "--------------------------------------"
+    Write-Host "Downloading Stinger"
+    $(New-Object System.Net.WebClient).DownloadFile("https://downloadcenter.mcafee.com/products/mcafee-avert/stinger/stinger64.exe","$env:LOCALAPPDATA\Temp\stinger64.exe")
+    if (Test-Path "$env:LOCALAPPDATA\Temp\stinger64.exe") {
+        Write-Host "Running Stinger"
+        Start-Process "$env:LOCALAPPDATA\Temp\stinger64.exe" "--silent --rename --reportpath=$($logPath)" -Wait -NoNewWindow
+        $stingerLog = Get-Content $(Get-ChildItem -Path $logPath -Recurse -Filter "Stinger*" | sort $_.LastWriteTime -Descending)[0].FullName
+        $stingerLog = $stingerLog -replace '<[^>]+>',"`n" -replace '&reg;','' -replace '&trade;','' -replace '&copy;',''
+        $stingerLog
+    } else {
+        Write-Host "Error downloading KVRT"
+    }
+}
+
+# ClamAV does not allow automatic download, thats why I saved it in GitHub.
+# If you want to download from another server or upgrade version: 
+#    1. Download ClamAV clamav-xxxx.win.x64.zip from https://www.clamav.net/downloads#otherversions.
+#    2. Extract all files and copy ..\clavav\conf_examples\freshclam.conf.sample to  \clavav\freshclam.conf
+#    3. Edit freshclam.conf and delete line 8: "Example"
+#    4. Create a zip with all ClamAV files in root.
+#    5. Upload zip to web server and change $clamavDownloadLocation variable below.
+if ($clamav -or $all -or $antivirus) {
+    $clamavDownloadLocation = "https://github.com/juangranados/powershell-scripts/blob/main/Windows%20Mainteinance/clamav-0.104.0.win.x64.zip?raw=true"
+    Write-Host "------------------------------"
+    Write-Host "Downloading and running ClamAV"
+    Write-Host "------------------------------"
+    $(New-Object System.Net.WebClient).DownloadFile($clamavDownloadLocation,"$env:LOCALAPPDATA\Temp\clamav.zip")
+    if (Test-Path "$env:LOCALAPPDATA\Temp\clamav.zip") {
+        Write-Host "Extracting clamav.zip"
+        Expand-Archive -LiteralPath "$env:LOCALAPPDATA\Temp\clamav.zip" "$env:LOCALAPPDATA\Temp\clamav" -Force
+        Remove-Item "$env:LOCALAPPDATA\Temp\clamav.zip" -Force
+        if (Test-Path "$env:LOCALAPPDATA\Temp\clamav\clamscan.exe") {
+            Write-Host "Updating ClamAV"
+            Start-Process "$env:LOCALAPPDATA\Temp\clamav\freshclam.exe" -Wait -NoNewWindow -RedirectStandardOutput "$($logPath)\$($timestamp)_$($env:COMPUTERNAME)_clamav_update.txt"
+            Get-Content -Path "$($logPath)\$($timestamp)_$($env:COMPUTERNAME)_clamav_update.txt"
+            Write-Host "Running ClamAV full scan of C:\"
+            Start-Process "$env:LOCALAPPDATA\Temp\clamav\clamscan.exe" "C:\ -r --remove=yes --infected --include=\.(exe|cmd|ps1|bat|dll|src|sys|msi|zip|rar|7z|cab|jar)$" -Wait -NoNewWindow -RedirectStandardOutput "$($logPath)\$($timestamp)_$($env:COMPUTERNAME)_clamav_scan.txt"
+            Get-Content -Path "$($logPath)\$($timestamp)_$($env:COMPUTERNAME)_clamav_scan.txt"
+        } else {
+            Write-Host "Error extracting ClamAV"
+        }
+    } else {
+        Write-Host "Error downloading ClamAV"
     }
 }
 if ($defrag -or $all) {
@@ -198,11 +264,18 @@ if ($update -or $all) {
     Write-Host "------------------"
     Write-Host "Installing Updates"
     Write-Host "------------------"
-    [Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12
-    Install-PackageProvider -Name NuGet -Force -Confirm:$false
-    Set-PSRepository -Name "PSGallery" -InstallationPolicy Trusted
-    Install-Module -Name PSWindowsUpdate -Force -Confirm:$false
+    if (-not (Get-InstalledModule PSWindowsUpdate)) {
+        Write-Host "Installing module PSWindowsUpdate"
+        [Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12
+        Install-PackageProvider -Name NuGet -Force -Confirm:$false
+        Set-PSRepository -Name "PSGallery" -InstallationPolicy Trusted
+        Install-Module -Name PSWindowsUpdate -Force -Confirm:$false
+    } else {
+        Write-Host "Updating module PSWindowsUpdate"
+        Update-Module PSWindowsUpdate
+    }
+    Write-Host "Running: Install-WindowsUpdate -MicrosoftUpdate -NotCategory 'Drivers' -AcceptAll -IgnoreUserInput -IgnoreReboot"
     Install-WindowsUpdate -MicrosoftUpdate -NotCategory "Drivers" -AcceptAll -IgnoreUserInput -IgnoreReboot
 }
 
-Stop-Transcript
+try{Stop-Transcript}catch{}
